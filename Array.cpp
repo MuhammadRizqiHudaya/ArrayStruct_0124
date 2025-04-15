b41d850c807b79227a51c597e2a2651998a81837
Muhammad Rizqi Hudaya <hudayamuhammadrizqi@gmail.com>
2025-04-15 19:23:16 +0700

    Add files via upload

---
+++ Array.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+//deklarasi array tanpa ukuran, wajib diisi data
+int nilai[]={1,2,3};
+
+
+int main() {
+    //menampilkan isi array pada index tertentu
+    cout << "tampilkan skor ke 3 = " << skor[2] << endl;
+    //mengisi array pada index tertentu 
+    skor[2] = 100;
+    cout << "tampilkan skor ke 3 =" << skor[2] << endl;
+
+    //mengisi array dengan loop for
+    for(int n = 0; n < 3; n++){
+        cout << "nama mahasiswa ke-" << n+1 << endl;
+        cout << "nama mahasiswa = ";
+        cin >> nama[n];
+    }
+    
+    cout << "menampilkan array dengan loop for" << endl;
+    for(int n = 0; n < 3; n++){
+        cout << "data ke-" << n+1 << endl;
+        cout << "data mahasiswa = " << nama[n] << endl;
+    }
+};
