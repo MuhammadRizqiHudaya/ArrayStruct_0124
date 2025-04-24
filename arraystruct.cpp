9572544d84b8f7f2676c4d71c9756eb565e94f1f
Muhammad Rizqi Hudaya <hudayamuhammadrizqi@gmail.com>
2025-04-24 11:39:59 +0700

    Update arraystruct.cpp

--- arraystruct.cpp
+++ arraystruct.cpp
@@ -17,27 +17,26 @@ int main(){
     mahasiswa mhs[2];
 
     for(int n = 0; n < 2; n++){
-    cout << "masukkan NIM ="; 
-    cin >> mhs[n].nim;
-    cin.ignore();
-    cout << "masukkan nama =";
-    getline(cin,  mhs[n].nama);
-    cin >> mhs[n].nama;
-    cout << "masukkan alamat desa =";
-    cin >> mhs[n].alamat.desa;
-    cout << "masukkan alamat kota =";
-    cin >> mhs[n].alamat.kota;
+        cout << "masukkan NIM ="; 
+        cin >> mhs[n].nim;
+        cin.ignore(); // Ignore the newline character left in the buffer
+        cout << "masukkan nama =";
+        getline(cin, mhs[n].nama); // Read the full name
+        cout << "masukkan alamat desa =";
+        cin >> mhs[n].alamat.desa;
+        cout << "masukkan alamat kota =";
+        cin >> mhs[n].alamat.kota;
     }
-    
 
     cout << endl;
     cout << "data mahasiswa" << endl;
 
     for(int n = 0; n < 2; n++){
-    cout << "NIM =" << mhs[n].nim << endl;
-    cout << "nama =" << mhs[n].nama << endl;
-    cout << "alamat desa =" << mhs[n].alamat.desa << endl;
-    cout << "alamat kota =" << mhs[n].alamat.kota << endl;
+        cout << "NIM =" << mhs[n].nim << endl;
+        cout << "nama =" << mhs[n].nama << endl;
+        cout << "alamat desa =" << mhs[n].alamat.desa << endl;
+        cout << "alamat kota =" << mhs[n].alamat.kota << endl;
     }
 
+    return 0;
 }
